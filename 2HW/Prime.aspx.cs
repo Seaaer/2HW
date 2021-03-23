cf50fc27e7cb8c29a5dab59a8007d45ba9d9a9d5
帛勳 謝 <s881230x@gmail.com>
2021-03-23 16:04:31 +0800

    1234

---
+++ 2HW/Prime.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int a = 20;
+            int b = 0, c = 0;
+            for(int i = 1; i <= a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    b = b + i;
+                }else if (i % 3 == 0)
+                {
+                    c = c + i;
+                }
+                
+            }
+            Response.Write(b + c);
+        }
+    }
+}
